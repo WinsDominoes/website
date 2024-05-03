270f341f0c3cf89f762eb05be158b6180e739174
WinsDominoes <winsdominoes2018@gmail.com>
2024-05-03 10:52:31 +0700

    changed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
-import NextNProgress from "nextjs-progressbar"
 
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import './globals.css';
 import './components.css';
 
+const inter = Inter({ subsets: ['latin'] });
+
 export const metadata: Metadata = {
-	title: "Thanawin Pattanaphol - ธนาวินทร์ พัฒนผล",
+	title: "Win Pattanaphol - Personal Website",
 	description: "A hobbyist full-stack developer, high schooler, enjoyer of 60s-80s music, language nerd, a hobbyist electrician from Thailand.",
 };
 
@@ -17,7 +19,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html className="text-black bg-white dark:text-white dark:bg-slate-900" lang="en">
+		<html className={`${inter.className} text-black bg-white dark:text-white dark:bg-slate-900`} lang="en">
 			<body className="antialiased mt-8 mb-8 lg:max-w-4xl min-lg:flex min-lg:flex-col lg:mx-auto max-lg:grid max-lg:place-items-center max-sm:place-items-start">
 				<Navbar />
 				{children}
